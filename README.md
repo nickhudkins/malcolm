dad4e8cea0f80df954fa57cde4fba691940421e4
Nick HudkinsName <nick@nickhudkins.com>
2024-04-06 00:41:57 -0400

    🎉 It's Alive

---
+++ README.md
@@ -0,0 +1,7 @@
+# 👴🏻 Malcolm (in the Middle)
+
+It's Man-In-The-Middle-Proxy for Development
+
+Clone and `npm run start`
+
+❤️ Made with Love by @nickhudkins
---
+++ src/proxy.ts
@@ -0,0 +1,56 @@
+import path from "path";
+import { spawn } from "child_process";
+import { parse } from "node-html-parser";
+import http from "http";
+
+import {
+  createProxyMiddleware,
+  responseInterceptor,
+} from "http-proxy-middleware";
+import { ProxyInitializationOptions } from "./types.js";
+
+export async function run({
+  proxyPort,
+  tlsTerminatorPort,
+  handleRequest,
+  handleResponse,
+  handleParsedHTML,
+}: ProxyInitializationOptions) {
+  return new Promise<void>((resolve) => {
+    const server = http.createServer(function (req, res) {
+      const middleware = createProxyMiddleware({
+        target: req.headers.origin,
+        changeOrigin: true,
+        selfHandleResponse: true,
+        on: {
+          proxyReq: (proxyReq, req, res, options) => {
+            handleRequest(proxyReq, req, res, options);
+          },
+          proxyRes: responseInterceptor(async (responseBuffer, proxyRes) => {
+            handleResponse(proxyRes, res);
+            // TODO: Check if it's something needing responsing, then do it.
+            if (proxyRes.headers["content-type"]?.startsWith("text/html")) {
+              const response = responseBuffer.toString("utf8"); // convert buffer to string
+              const root = parse(response);
+              return handleParsedHTML(root).toString();
+            }
+            return responseBuffer;
+          }),
+        },
+      });
+      middleware(req, res);
+    });
+
+    server.listen(proxyPort, () => {
+      const caddyProc = spawn("caddy", [
+        `reverse-proxy`,
+        `--from`,
+        `localhost:${tlsTerminatorPort}`,
+        `--to`,
+        `localhost:${proxyPort}`,
+      ]);
+      caddyProc.stdout.on("data", (chunk) => console.log(chunk.toString()));
+      resolve();
+    });
+  });
+}
