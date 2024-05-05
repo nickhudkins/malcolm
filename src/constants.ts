4de16f2b2633c9e14d1444bd982593f26684d4a2
Nick Hudkins <nhudkins@amazon.com>
2024-05-04 21:05:43 -0400

    🥴 Watching TV, Don't Recall

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+import { homedir } from "os";
+import { join } from "path";
+
+const home = homedir();
+export const DEFAULT_CONFIG_DIR = join(home, ".malcolm");
+
+export const DEFAULT_CERT_PATH = join(DEFAULT_CONFIG_DIR, "malcom-cert.pem");
+export const DEFAULT_KEY_PATH = join(DEFAULT_CONFIG_DIR, "malcolm-key.pem");
+
+export const DEFAULT_PROXY_PORT = 6969;
