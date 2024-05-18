18fadda5c483200b560c4e84c1f0a8882e40d8d3
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-05-18 13:19:14 -0500

    🙌 Finally working

--- src/cli.ts
+++ src/cli.ts
@@ -3,30 +3,51 @@ import getPort from "get-port";
 import { join } from "path";
 import { run } from "./proxy.js";
 import { tsImport } from "tsx/esm/api";
+import { promises as fs } from "fs";
 import { DEFAULT_PROXY_PORT } from "./constants.js";
 
 const proxyPort = await getPort({
   port: DEFAULT_PROXY_PORT,
 });
 
-function noop() {}
+// create the server
+const configure = await run();
+
+function noop() { }
+
+async function loadConfig() {
+
+  const userConfig = await tsImport(userConfigPath, import.meta.url);
+
+  const {
+    default: {
+      handleRequest = noop,
+      handleResponse = noop,
+      shouldIntercept,
+      hosts = [],
+    },
+  } = userConfig;
+
+  await configure({
+    hosts,
+    proxyPort,
+    shouldIntercept,
+    handleResponse,
+    handleRequest,
+  });
+
+}
 
 const userConfigPath = join(process.cwd(), "proxy.config.ts");
-const userConfig = await tsImport(userConfigPath, import.meta.url);
+await loadConfig()
 
-const {
-  default: {
-    handleRequest = noop,
-    handleResponse = noop,
-    shouldIntercept,
-    hosts = [],
-  },
-} = userConfig;
-
-await run({
-  hosts,
-  proxyPort,
-  shouldIntercept,
-  handleResponse,
-  handleRequest,
-});
+// attach a watcher to this file
+try {
+  const configWatcher = fs.watch(userConfigPath);
+  for await (const event of configWatcher) {
+    if (event.eventType !== "change") continue;
+    loadConfig()
+  }
+} catch (err: any) {
+  if (err.name === "AbortError") throw err;
+}
