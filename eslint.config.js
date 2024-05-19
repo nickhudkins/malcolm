d56d009655e3d1686f179558ce529e0d7217a5b7
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-05-19 08:52:58 -0500

    😂 Wow only one flat file

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,11 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+// NOTE: v9 is way diff
+// https://eslint.org/blog/2022/08/new-config-system-part-1/
+// https://eslint.org/blog/2022/08/new-config-system-part-2/
+
+/** @type {import('typescript-eslint').Config} */
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
