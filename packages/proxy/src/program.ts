6a710fafcdd2de8ff828a20b64b0de67fc1f135f
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-05-28 12:40:57 -0500

    👨🔬 "use the platform"

--- packages/proxy/src/program.ts
+++ packages/proxy/src/program.ts
@@ -2,10 +2,9 @@ import getPort from "get-port";
 import { getProxyConfig } from "./config.js";
 import { createFileWatcher } from "./utils.js";
 import { create } from "./proxy.js";
-import fs from "fs";
 import path from "path";
 
-import { spawn, fork } from "child_process";
+import { unsetProxy } from "./system.js";
 
 interface ProgramOptions {
   proxyPort: number;
@@ -21,16 +20,16 @@ export async function program(opts: ProgramOptions) {
 
   const start = await create({ port: proxyPort });
 
-  const stopWatching = createFileWatcher(fullyQualifiedPath, run);
+  createFileWatcher(fullyQualifiedPath, run);
   // Wrap up getting config and starting
   async function run() {
     const config = await getProxyConfig(fullyQualifiedPath);
-    const server = await start(config);
+    await start(config);
 
-    const f = fs.openSync("log.out", "a");
     process.on("SIGINT", async () => {
-      const thing = spawn("npx tsx src/cleanup.ts", { shell: true, detached: true, stdio: ["ignore", f, f, "ipc"] });
-      thing.unref();
+      await unsetProxy();
+
+      process.exit(0);
     });
   }
   await run();
