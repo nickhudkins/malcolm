ead8ea146cfec5a729f8d3dc0e8b658da4f97800
Kirin Patel <kirinpatel@gmail.com>
2024-06-09 00:01:09 -0400

    😎 get linux working (kinda)

--- packages/proxy/src/system/linux.ts
+++ packages/proxy/src/system/linux.ts
@@ -1,10 +1,35 @@
+// import { execSync } from "child_process";
+
 import type { MalcolmSystemFunctions } from "./system.js";
 
 // TODO: Impelemnt, maybe fix types too!
 export default {
-  buildRemoveCertificateCommand(_certName) {},
-  buildSetProxyCommand(_networkInterfaceName, _proxyPort) {},
-  buildTrustCertificateCommand(_certName) {},
-  buildUnsetProxyCommand(_networkInterfaceName) {},
-  getNetworkInterfaceName(_interfaceName) {},
+  buildRemoveCertificateCommand(_certName) {
+    return "sudo update-ca-certificates --fresh";
+  },
+  buildSetProxyCommand(_networkInterfaceName, _proxyPort) {
+    // TODO: Figure out what needs to be done here
+    return "echo Hello";
+  },
+  buildTrustCertificateCommand(_certName) {
+    return "sudo update-ca-certificates";
+  },
+  buildUnsetProxyCommand(_networkInterfaceName) {
+    // TODO: Figure out what needs to be done here
+    return "echo Hello";
+  },
+  getNetworkInterfaceName(interfaceName) {
+    // TODO: Figure out what needs to be done here
+    // const allNetworkIntefaces = execSync(`ip link show`).toString().split(/^n/im);
+
+    // let cleanedName;
+    // allNetworkIntefaces.filter(networkInterface => {
+    //   const foundInteface = networkInterface.includes(interfaceName);
+
+    //   if (foundInteface) {
+    //     const [_index, dirtyName] = networkInterface.split(" ");
+    //   }
+    // });
+    return interfaceName;
+  },
 } as MalcolmSystemFunctions;
