7015b61c41cda8c8c9e26cb7142930c3bc5c60b0
Kirin Patel <kirinpatel@gmail.com>
2024-06-08 23:39:51 -0400

    🔧 improve NetworkInterface types

--- packages/proxy/src/network.d.ts
+++ packages/proxy/src/network.d.ts
@@ -1,6 +1,13 @@
 declare module "network" {
   interface NetworkInterface {
-    desc: string;
+    name: string;
+    desc?: string;
+    type?: string;
+    ip_address: string;
+    mac_address: string;
+    gateway_ip: string;
+    netmask: string;
+    status?: string;
   }
 
   export const get_active_interface: (cb: (err: Error, interface: NetworkInterface) => void) => void;
